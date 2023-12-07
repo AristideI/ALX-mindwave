e22292d20dfa349fac94d58305c40dcdc85f3e7d
AristideI <a.isingizwe@alustudent.com>
2023-12-07 18:25:52 +0200

    latest feature

--- src/components/MainProtectedLayout.jsx
+++ src/components/MainProtectedLayout.jsx
@@ -1,19 +1,64 @@
-import { Link } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import LogoIcon from "../assets/LogoIcon";
 
 export default function MainProtectedLayout() {
   return (
     <article className="relative flex justify-end text-light-200">
-      <section className="w-1/5 h-screen fixed left-0 top-0 pl-6 py-12">
+      <section className="w-1/5 h-screen fixed left-0 top-0 px-6 py-12 flex flex-col justify-between border-r-2 border-light-200">
         <section className="flex justify-start items-center gap-4 font-serif font-bold text-xl">
           <LogoIcon classes="text-dark-100 w-10 h-10" />
           <p className="text-2xl">Mind Wave</p>
         </section>
-        <section>
-          <Link to="/feeds">Home</Link>
+        <section className="flex flex-col gap-3">
+          <NavLink
+            to="/feeds"
+            className="flex items-center gap-6 py-2 hover:bg-dark-100 hover:text-dark-200"
+          >
+            <img
+              className="w-8 object-contain"
+              src="./public/home.png"
+              alt="home icon"
+            />
+            <p className="font-semibold text-xl">Home</p>
+          </NavLink>
+          <NavLink
+            to="/quiz"
+            className="flex items-center gap-6 py-2 hover:bg-dark-100 hover:text-dark-200"
+          >
+            <img
+              className="w-8 object-contain"
+              src="./public/quizzes.png"
+              alt="Quiz icon"
+            />
+            <p className="font-semibold text-xl">Quizzes</p>
+          </NavLink>
+          <NavLink
+            to="/daily"
+            className="flex items-center gap-6 py-2 hover:bg-dark-100 hover:text-dark-200"
+          >
+            <img
+              className="w-8 object-contain"
+              src="./public/motivation.png"
+              alt="Motivationo icon"
+            />
+            <p className="font-semibold text-xl">Motivation</p>
+          </NavLink>
+        </section>
+        <section className="flex items-center gap-6">
+          <img
+            className="w-16 h-16 rounded-full object-cover"
+            src="./public/sample.jpg"
+            alt="profile img"
+          />
+          <div>
+            <p className="font-bold text-xl">Aristide Isingizwe</p>
+            <p>@AngryWolf</p>
+          </div>
         </section>
       </section>
-      <section className="w-4/5 h-screen bg-green-500">hero</section>
+      <section className="w-4/5">
+        <Outlet />
+      </section>
     </article>
   );
 }
--- src/pages/Feeds.jsx
+++ src/pages/Feeds.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+
 export default function Feeds() {
-  return <h1>This is feeds</h1>;
+  const [postData, setPostData] = useState({ post: "", mood: "" });
+  console.log(postData);
+  function handleFormChange(e) {
+    setPostData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+  function handlePosting() {}
+  return (
+    <article>
+      <section className="p-12 flex gap-4 w-full border-b border-light-200">
+        <img
+          className="w-12 h-12 rounded-full object-cover"
+          src="./public/sample.jpg"
+          alt=""
+        />
+        <form className="w-full">
+          <input
+            className="placeholder:text-xl text-light-200 bg-transparent py-2 w-full active:bg-transparent focus:bg-transparent outline-none"
+            type="text"
+            placeholder="What's on your mind today ?"
+            name="post"
+            value={postData.post}
+            onChange={(e) => handleFormChange(e)}
+          />
+          <div className="w-full flex justify-between items-center mt-4">
+            <select
+              name="mood"
+              className="bg-transparent px-4 py-2 border rounded-lg"
+              onChange={(e) => handleFormChange(e)}
+            >
+              <option value="">Whats Your Mood today ?</option>
+              <option value="happy">Happy</option>
+              <option value="sad">Sad</option>
+              <option value="depressed">Worried</option>
+              <option value="worried">Worried</option>
+            </select>
+            <button
+              className="bg-dark-100 px-8 py-2 text-dark-200 text-xl font-semibold"
+              onClick={(e) => {
+                e.preventDefault();
+                handlePosting;
+              }}
+            >
+              Post
+            </button>
+          </div>
+        </form>
+      </section>
+      <section></section>
+    </article>
+  );
 }
