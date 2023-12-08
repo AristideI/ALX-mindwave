280907d3fdda722fd67da03e9c7b9e4ff1be9711
AristideI <a.isingizwe@alustudent.com>
2023-12-08 16:27:59 +0200

    meet the team

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import Hero from "../components/Hero";
+import Meet from "../components/Meet";
 
 export default function Home() {
   return (
     <section>
       <Hero />
       <About />
+      <Meet />
     </section>
   );
 }
