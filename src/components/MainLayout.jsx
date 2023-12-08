6a526b2fdeec74f10fd50f09ca200f60ab848e5b
AristideI <a.isingizwe@alustudent.com>
2023-12-08 12:46:28 +0200

    scroll behavior

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function MainLayout() {
   return (
-    <section className="bg-dark-200 text-light-200 px-[5%] text-xl">
+    <section className="bg-dark-200 text-light-200 px-[5%]">
       <Header />
       <Outlet />
     </section>
